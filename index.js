eea73431f5090b443a7b79bc51c73087dc3c74ec
Bluzzi <camilledugas.pro@gmail.com>
2023-02-05 13:31:42 +0100

    remove Tailwind rules

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:tailwindcss/recommended"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint/eslint-plugin"],
   ignorePatterns: [".eslintrc.js", "dist", "build"],
