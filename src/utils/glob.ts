2eaa7cf121a6647291f3e0bf1134118853e667b9
Bluzzi <camilledugas.pro@gmail.com>
2023-11-09 07:30:11 +0100

    refactor(glob.ts): remove unused glob patterns and consolidate related patterns
    The following changes were made:
    - Removed unused glob patterns for styles (GLOB_STYLE, GLOB_CSS, GLOB_LESS, GLOB_SCSS)
    - Removed unused glob patterns for markdown (GLOB_MARKDOWN)
    - Removed unused glob patterns for other file types (GLOB_VUE, GLOB_YAML, GLOB_HTML)
    - Consolidated all source file glob patterns into a single array (GLOB_ALL_SRC)
    - Removed unused glob pattern for markdown code (GLOB_MARKDOWN_CODE)
    - Removed unused glob pattern for excluding directories (GLOB_EXCLUDE)

--- src/utils/glob.ts
+++ src/utils/glob.ts
@@ -7,21 +7,9 @@ export const GLOB_JSX = "**/*.?([cm])jsx";
 export const GLOB_TS = "**/*.?([cm])ts";
 export const GLOB_TSX = "**/*.?([cm])tsx";
 
-export const GLOB_STYLE = "**/*.{c,le,sc}ss";
-export const GLOB_CSS = "**/*.css";
-export const GLOB_LESS = "**/*.less";
-export const GLOB_SCSS = "**/*.scss";
-
 export const GLOB_JSON = "**/*.json";
 export const GLOB_JSON5 = "**/*.json5";
 export const GLOB_JSONC = "**/*.jsonc";
-
-export const GLOB_MARKDOWN = "**/*.md";
-export const GLOB_VUE = "**/*.vue";
-export const GLOB_YAML = "**/*.y?(a)ml";
-export const GLOB_HTML = "**/*.htm?(l)";
-
-export const GLOB_MARKDOWN_CODE = `${GLOB_MARKDOWN}/${GLOB_SRC}`;
 
 export const GLOB_TESTS = [
   `**/__tests__/**/*.${GLOB_SRC_EXT}`,
@@ -29,17 +17,6 @@ export const GLOB_TESTS = [
   `**/*.test.${GLOB_SRC_EXT}`,
   `**/*.bench.${GLOB_SRC_EXT}`,
   `**/*.benchmark.${GLOB_SRC_EXT}`
-];
-
-export const GLOB_ALL_SRC = [
-  GLOB_SRC,
-  GLOB_STYLE,
-  GLOB_JSON,
-  GLOB_JSON5,
-  GLOB_MARKDOWN,
-  GLOB_VUE,
-  GLOB_YAML,
-  GLOB_HTML
 ];
 
 export const GLOB_EXCLUDE = [
