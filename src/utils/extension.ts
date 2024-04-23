499717c9e4cb834c9d2ae5fcde3efe2e22c78589
Bluzzi <camilledugas.pro@gmail.com>
2024-04-23 16:03:08 +0200

    style(extension.ts): add newline at end of file for POSIX compliance

--- src/utils/extension.ts
+++ src/utils/extension.ts
@@ -2,4 +2,5 @@ export { default as antfuPlugin } from 'eslint-plugin-antfu'
 export { default as nodePlugin } from 'eslint-plugin-n'
 export { default as stylisticPlugin } from '@stylistic/eslint-plugin'
 export { default as typescriptPlugin } from '@typescript-eslint/eslint-plugin'
+
 export { default as typescriptParser } from '@typescript-eslint/parser'
