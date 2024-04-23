dccdf671af13a03bb4112f1528622ef70c95eaa5
Bluzzi <camilledugas.pro@gmail.com>
2024-04-23 11:03:41 +0200

    refactor(index.ts): remove unused eslint utils export refactor(util.ts): remove parserPlain and renamePluginInConfigs functions due to redundancy

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ export * from "#/configs/node";
 export * from "#/configs/stylistic";
 
 // Utils:
-export * from "#/utils/eslint";
 export * from "#/utils/factory";
 export * from "#/utils/glob";
 export * from "#/utils/plugin";
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -2,27 +2,6 @@ import process from 'node:process'
 import { isPackageExists } from 'local-pkg'
 import type { Awaitable, TypedFlatConfigItem } from '#/types/type'
 
-export const parserPlain = {
-  meta: {
-    name: 'parser-plain',
-  },
-  parseForESLint: (code: string) => ({
-    ast: {
-      body: [],
-      comments: [],
-      loc: { end: code.length, start: 0 },
-      range: [0, code.length],
-      tokens: [],
-      type: 'Program',
-    },
-    scopeManager: null,
-    services: { isPlain: true },
-    visitorKeys: {
-      Program: [],
-    },
-  }),
-}
-
 /**
  * Combine array and non-array configs into a single array.
  */
@@ -60,39 +39,6 @@ export function renameRules(rules: Record<string, any>, map: Record<string, stri
         return [key, value]
       }),
   )
-}
-
-/**
- * Rename plugin names a flat configs array
- *
- * @example
- * ```ts
- * import { renamePluginInConfigs } from '@antfu/eslint-config'
- * import someConfigs from './some-configs'
- *
- * export default renamePluginInConfigs(someConfigs, {
- *   '@typescript-eslint': 'ts',
- *   'import-x': 'import',
- * })
- * ```
- */
-export function renamePluginInConfigs(configs: TypedFlatConfigItem[], map: Record<string, string>): TypedFlatConfigItem[] {
-  return configs.map((i) => {
-    const clone = { ...i }
-    if (clone.rules)
-      clone.rules = renameRules(clone.rules, map)
-    if (clone.plugins) {
-      clone.plugins = Object.fromEntries(
-        Object.entries(clone.plugins)
-          .map(([key, value]) => {
-            if (key in map)
-              return [map[key], value]
-            return [key, value]
-          }),
-      )
-    }
-    return clone
-  })
 }
 
 export function toArray<T>(value: T | T[]): T[] {
