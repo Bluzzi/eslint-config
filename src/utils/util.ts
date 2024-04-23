594ef9642196fe922077bffece3cf5d341963d8a
Bluzzi <camilledugas.pro@gmail.com>
2024-04-23 12:40:42 +0200

    chore(package.json): remove unused dependencies @antfu/install-pkg, @clack/prompts, and @eslint/config-inspector refactor(util.ts): remove ensurePackages function to simplify package management logic

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -44,19 +44,3 @@ export function renameRules(rules: Record<string, any>, map: Record<string, stri
 export function toArray<T>(value: T | T[]): T[] {
   return Array.isArray(value) ? value : [value]
 }
-
-export async function ensurePackages(packages: (string | undefined)[]) {
-  if (process.env.CI || process.stdout.isTTY === false)
-    return
-
-  const nonExistingPackages = packages.filter(i => i && !isPackageExists(i)) as string[]
-  if (nonExistingPackages.length === 0)
-    return
-
-  const p = await import('@clack/prompts')
-  const result = await p.confirm({
-    message: `${nonExistingPackages.length === 1 ? 'Package is' : 'Packages are'} required for this config: ${nonExistingPackages.join(', ')}. Do you want to install them?`,
-  })
-  if (result)
-    await import('@antfu/install-pkg').then(i => i.installPackage(nonExistingPackages, { dev: true }))
-}
