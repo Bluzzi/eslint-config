f50ce650c85029ce35ff7cee4fff8beee8e84d87
Bluzzi <camilledugas.pro@gmail.com>
2024-04-24 14:38:43 +0200

    feat(package.json): upgrade eslint-plugin-n to ^17.3.0 for improved linting rules refactor(src/index.ts): streamline exports by removing unnecessary configs, utils, and types to simplify module exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-// Configs:
-export * from "#/configs/typescript/config";
-export * from "#/configs/javascript/config";
-export * from "#/configs/node";
-export * from "#/configs/stylistic/config";
-
-// Utils:
 export * from "#/utils/factory";
-export * from "#/utils/extension";
-export * from "#/utils/util";
-
-// Types:
-export * from "#/types/type";
